57860f852cb3f5a714356161e7f9cc6dd56cf6b6
Arelse <verizyves@gmail.com>
2026-09-22 21:28:09 +0530

    Add MainActivity for app entry point

---
+++ app/src/main/java/com/kaimono/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kaimono.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kaimono.app.ui.AppRoot
+import com.kaimono.app.ui.AppViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val vm: AppViewModel = viewModel()
+            AppRoot(vm)
+        }
+    }
+}
+
